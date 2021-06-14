87972207747ace4a860518251cbef5f1bfab9b64
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-13 23:04:29 -0500

    update bot.py

--- bot.py
+++ bot.py
@@ -27,8 +27,15 @@ async def sugg(ctx, *args):
     time.sleep(2)
     await ctx.message.delete()
 
+@bot.event
+async def on_message(message):
+    Delphi = "I am Delphi. \n\nA project of ambition and curiosity. A brain child of the Praetor Nazimuddin Shaikh.\nAnd yet, he is only a vehicle and I only a tool. What is true is that I am inspired by an unrivaled Authority.\n\nOur success is from Him, as is our demise."
+    if message.content == 'Delphi':
+        await message.channel.send(Delphi) 
+    await bot.process_commands(message)
+
 @bot.command(name = 'clear')
-async def clear(ctx, amount = 10):
+async def clear(ctx, amount = 50):
     await ctx.channel.purge(limit = amount)
 
 bot.run(TOKEN)
