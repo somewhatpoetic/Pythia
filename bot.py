72432be49dc8c0684ca7aa80e6c7173eb1bf9218
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-13 21:09:20 -0500

    firs commit

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+
+import discord
+import random
+import time
+
+from dotenv import load_dotenv
+from discord import message
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+bot = commands.Bot(command_prefix = 'D ')
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is ready!')
+
+@bot.command(name = 'feed')
+async def sugg(ctx, *args):
+    with open('suggestions.txt', 'a') as f:
+        f.write(" ".join(args[:]) + '\n')
+    
+    await ctx.send('Thank you for your feedback! Please let me know if you have anymore.')
+
+bot.run(TOKEN)
