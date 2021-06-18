4630e4581e77cfc3b5fdfca6ff5d482d7037a8ad
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-17 21:06:45 -0500

    move poll command to a cog file

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import os
 import time
 import discord
 from discord import message
+from discord import client
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -10,11 +11,27 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix = 'D ')
+bot = commands.Bot(command_prefix = ['D ', 'd ', '.', '!'])
+
+# load extension
+@bot.command(hidden = True)
+async def load(ctx, extension):
+    bot.load_extension(f'cogs.{extension}')
+
+# unload extension
+@bot.command(hidden = True)
+async def unload(ctx, extension):
+    bot.unload_extension(f'cogs.{extension}')
+
+# loading all extensions
+for fname in os.listdir('./cogs'):
+    if fname.endswith('.py'):
+        bot.load_extension(f'cogs.{fname[:-3]}')
+
 
 # Commands -----------------------------------
 # feedback command
-@bot.command(name = 'feed')
+@bot.command()
 async def feed(ctx, *args):
     with open('suggestions.txt', 'a') as f:
         f.write(" ".join(args[:]) + '\n')
@@ -26,32 +43,6 @@ async def feed(ctx, *args):
 @bot.command(name = 'clear')
 async def clear(ctx, amount = 50):
     await ctx.channel.purge(limit = amount)
-
-# run a poll command
-@bot.command(name = 'poll', pass_context = True)
-async def poll(ctx, question, *options: str):
-    if len(options) <= 1:
-        await ctx.send('You need more than one option to make a poll!')
-        return
-    if len(options) > 10:
-        await ctx.send('You cannot make a poll for more than 10 things!')
-        return
-
-    if len(options) == 2 and options[0] == 'yes' and options[1] == 'no':
-        reactions = ['✅', '❌']
-    else:
-        reactions = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣', '🔟']
-
-    description = []
-    for x, option in enumerate(options):
-        description += '\n {} {}'.format(reactions[x], option)
-    msg = discord.Embed(title = question, description = ''.join(description))
-    react_message = await ctx.send(embed=msg)
-    for i in range(len(options)):
-        await react_message.add_reaction(reactions[i])
-    msg.set_footer(text='Poll ID: {}'.format(react_message.id))
-    await bot.edit_message(react_message, embed=msg)
-
 
 # Events -------------------------------------
 # on ready
