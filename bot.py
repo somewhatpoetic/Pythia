55ea3706b191dc1ae73dedb0e5fe14fb7ac21687
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-21 12:24:48 -0500

    move clear and feedback commands to misc.py

--- bot.py
+++ bot.py
@@ -1,8 +1,4 @@
 import os
-import time
-import discord
-from discord import message
-from discord import client
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -18,33 +14,11 @@ bot = commands.Bot(command_prefix = ['.'])
 async def load(ctx, extension):
     bot.load_extension(f'cogs.{extension}')
 
-# unload extension
-@bot.command(hidden = True)
-async def unload(ctx, extension):
-    bot.unload_extension(f'cogs.{extension}')
-
 # loading all extensions
 for fname in os.listdir('./cogs'):
     if fname.endswith('.py'):
         bot.load_extension(f'cogs.{fname[:-3]}')
 
-
-# Commands -----------------------------------
-# feedback command
-@bot.command()
-async def feed(ctx, *args):
-    with open('suggestions.txt', 'a') as f:
-        f.write(" ".join(args[:]) + '\n')
-    await ctx.send('Thank you for your feedback! Please let me know if you have anymore.', delete_after = 3)
-    time.sleep(2)
-    await ctx.message.delete()
-
-# clear command
-@bot.command(name = 'clear')
-async def clear(ctx, amount = 50):
-    await ctx.channel.purge(limit = amount)
-
-# Events -------------------------------------
 # on ready
 @bot.event
 async def on_ready():
