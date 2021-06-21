00df917d4fc3d1f68c7c3e5a63bc1277bbd36a56
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-21 15:32:06 -0500

    add amount arg to delete messages command

--- cogs/misc.py
+++ cogs/misc.py
@@ -8,7 +8,7 @@ class Misc(commands.Cog):
 
     # feedback command
     @commands.command()
-    async def feed(ctx, *args):
+    async def feed(self, ctx, *args):
         with open('suggestions.txt', 'a') as f:
             f.write(" ".join(args[:]) + '\n')
         await ctx.send('Thank you for your feedback! Please let me know if you have anymore.', delete_after = 3)
@@ -16,9 +16,9 @@ class Misc(commands.Cog):
         await ctx.message.delete()
 
     # clear command
-    @commands.command(name = 'clear')
-    async def clear(ctx, amount = 50):
-        await ctx.channel.purge(limit = amount)
+    @commands.command()
+    async def clear(self, ctx, amt: int):
+        await ctx.channel.purge(limit = amt)
         
 def setup(bot):
     bot.add_cog(Misc(bot))
