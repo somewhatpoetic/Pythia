786ae0e0a5fcc72647b3bc9f2e3c2691ee033f93
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-27 01:24:30 -0500

    implement urduTTS.py

--- cogs/UrduTTS.py
+++ cogs/UrduTTS.py
@@ -1,4 +1,7 @@
+
+import gtts
 from discord.ext import commands
+from playsound import playsound
 
 
 class UrduTTS(commands.Cog):
@@ -6,9 +9,18 @@ class UrduTTS(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.Cog.listener
+    @commands.Cog.listener()
     async def on_ready(self):
         print('UrduTTS is ready.')
+
+    @commands.command()
+    async def urduTTS(self, ctx):
+        userText = ctx.message.content
+        text = userText.replace('.urduTTS ', '')
+        audio = gtts.gTTS(text=text, lang='ur')
+        audio.save('urduTTS.mp3')
+
+        playsound('urduTTS.mp3')
 
 
 def setup(bot):
