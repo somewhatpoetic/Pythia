b3a678c57bb7a56c3241479b30148b3745d88852
somewhatpoetic <nazimuddin.shaikh2000@gmail.com>
2021-06-29 16:35:38 -0500

    fix spongebob text generator

--- cogs/spongebobText.py
+++ cogs/spongebobText.py
@@ -4,6 +4,30 @@ from discord.ext.commands import command
 from discord.ext.commands import Cog
 
 
+def removeSpace(list):
+    newList = [i for i in list if i != ' ']
+    return newList
+
+
+def getSpaceIndex(list):
+    spaces = [i for i, letter in enumerate(list) if letter == ' ']
+    return spaces
+
+
+def addSpacesBack(list, spaces):
+    for i in range(0, len(list) + len(spaces)):
+        if i in spaces:
+            list.insert(i, ' ')
+    return list
+
+
+def camelify(list):
+    for i in range(0, len(list)):
+        if i % 2 == 0:
+            list[i] = list[i].upper()
+    return list
+
+
 class SpongebobText(Cog):
 
     def __init__(self, bot):
@@ -19,21 +43,12 @@ class SpongebobText(Cog):
         lower = message.lower()
         letters = list(lower)
 
-        """
-        letters = ['M', 'y', ' ', 'n', 'a', 'm', 'e', ' ']
-        """
+        spaces = getSpaceIndex(letters)
+        spacesRemovedLetters = removeSpace(letters)
+        camelifiedLetters = camelify(spacesRemovedLetters)
+        lettersHalf = addSpacesBack(camelifiedLetters, spaces)
 
-        offset = 0
-        for i in range(0, len(letters)):
-            i += offset
-            if (i % 2 == 0) and (i < len(letters)):
-                if letters[i] == ' ':
-                    letters[i+1] = letters[i+1].upper()
-                    offset += 1
-                else:
-                    letters[i] = letters[i].upper()
-
-        sbText = ''.join(letters)
+        sbText = ''.join(lettersHalf)
         await ctx.send(sbText)
 
         time.sleep(3)
